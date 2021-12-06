69e4c8d2c1f849219526955efeaec3aed5567390
Sjoerd Simons <sjoerd@collabora.com>
2021-12-06 22:37:28 +0100

    ust some fun

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,136 +1,185 @@
+use anyhow::{Context, Result};
 use std::cmp;
 use std::collections::HashSet;
-use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
 
-fn main() {
-    let file = File::open("05.txt").unwrap();
+fn main() -> Result<()> {
+    let file = File::open("05.txt")?;
     let mut grid_points = HashSet::new();
     let mut danger_points = HashSet::new();
+
     for input_line in BufReader::new(file).lines() {
-        match input_line {
-            Ok(input_line) => {
-                let line = parse_line(&input_line).unwrap();
-                if line.is_horizontal() || line.is_vertical() {
-                    for point in line.points() {
-                        let key = (point.x, point.y);
-                        if grid_points.contains(&key) {
-                            danger_points.insert(key);
-                        }
-                        grid_points.insert(key);
-                    }
+        let line: Line = input_line?.parse()?;
+        if let Some(points) = line.points() {
+            for point in points {
+                if grid_points.contains(&point) {
+                    danger_points.insert(point);
                 }
+                grid_points.insert(point);
             }
-            Err(_) => ()
         }
     }
-    println!("{:?}", danger_points);
+    println!("{:#?}", danger_points);
     println!("{}", danger_points.len());
+
+    Ok(())
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, Copy)]
 pub struct Point {
-  pub x: u32,
-  pub y: u32,
+    pub x: u32,
+    pub y: u32,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct Line {
-  pub x1: u32,
-  pub y1: u32,
-  pub x2: u32,
-  pub y2: u32,
-}
+impl FromStr for Point {
+    type Err = anyhow::Error;
 
-trait Orientation {
-    fn is_horizontal(&self) -> bool;
-    fn is_vertical(&self) -> bool;
-}
-
-trait DiscreteLine {
-    fn points(&self) -> Vec<Point>;
-}
-
-impl Orientation for Line {
-    fn is_horizontal(&self) -> bool { self.y1 == self.y2 }
-    fn is_vertical(&self) -> bool { self.x1 == self.x2 }
-}
-
-impl DiscreteLine for Line {
-    fn points(&self) -> Vec<Point> {
-        let xmin = cmp::min(self.x1, self.x2);
-        let xmax = cmp::max(self.x1, self.x2);
-        let mut xrange = xmin..=xmax;
-
-        let ymin = cmp::min(self.y1, self.y2);
-        let ymax = cmp::max(self.y1, self.y2);
-        let mut yrange = ymin..=ymax;
-
-        let mut points: Vec<Point> = Vec::new();
-        let mut x: u32 = 0;
-        let mut y: u32 = 0;
-        loop {
-            x = xrange.next().unwrap_or(x);
-            y = yrange.next().unwrap_or(y);
-            points.push(Point {x: x, y: y});
-            if x == xmax && y == ymax {
-                break
-            }
-        }
-        points
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y) = s.split_once(',').context("Failed to split point")?;
+        Ok(Point {
+            x: x.parse()?,
+            y: y.parse()?,
+        })
     }
 }
 
-fn parse_point(input: &str) -> Result<Point, &str> {
-  let mut components = input.splitn(2, ",");
-  let x: u32 = components.next().unwrap().parse().unwrap();
-  let y: u32 = components.next().unwrap().parse().unwrap();
-  Ok(Point {x: x, y: y})
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        let ord_x = self.x.cmp(&other.x);
+        let ord_y = self.y.cmp(&other.y);
+        match (ord_x, ord_y) {
+            (ord_x, ord_y) if ord_x == ord_y => Some(ord_x),
+            (ord_x, cmp::Ordering::Equal) => Some(ord_x),
+            (cmp::Ordering::Equal, ord_y) => Some(ord_y),
+            _ => None,
+        }
+    }
 }
 
-fn parse_line(input: &str) -> Result<Line, &str> {
-  let mut components = input.splitn(2, " -> ");
-  let origin = parse_point(components.next().unwrap()).unwrap();
-  let destination = parse_point(components.next().unwrap()).unwrap();
-  Ok(Line {x1: origin.x, y1: origin.y, x2: destination.x, y2: destination.y})
+#[derive(Debug, PartialEq)]
+pub enum Orientation {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct Line {
+    start: Point,
+    end: Point,
+}
+
+impl Line {
+    pub fn start(&self) -> Point {
+        self.start
+    }
+
+    pub fn end(&self) -> Point {
+        self.end
+    }
+
+    pub fn orientation(&self) -> Option<Orientation> {
+        if self.start.x == self.end.x {
+            Some(Orientation::Horizontal)
+        } else if self.start.y == self.end.y {
+            Some(Orientation::Vertical)
+        } else {
+            None
+        }
+    }
+
+    pub fn points(&self) -> Option<DiscreteLine> {
+        if self.orientation().is_some() {
+            Some(DiscreteLine {
+                remaining: Some(*self),
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl FromStr for Line {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (p0, p1) = s.split_once(" -> ").context("Failed to split line")?;
+        let p0 = p0.parse()?;
+        let p1 = p1.parse()?;
+
+        if p0 < p1 {
+            Ok(Line { start: p0, end: p1 })
+        } else {
+            Ok(Line { start: p1, end: p0 })
+        }
+    }
+}
+
+pub struct DiscreteLine {
+    remaining: Option<Line>,
+}
+
+impl Iterator for DiscreteLine {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let remaining = self.remaining.as_mut()?;
+        let item = remaining.start;
+        if remaining.start == remaining.end {
+            self.remaining = None;
+        } else {
+            match remaining.orientation().expect("No orientation") {
+                Orientation::Horizontal => remaining.start.y += 1,
+                Orientation::Vertical => remaining.start.x += 1,
+            }
+        }
+        Some(item)
+    }
 }
 
 #[test]
 fn test_parse_line() {
-  assert_eq!(parse_line("6,4 -> 2,0"), Ok(Line {
-    x1: 6,
-    y1: 4,
-    x2: 2,
-    y2: 0,
-  }));
+    assert_eq!(
+        "6,4 -> 2,0".parse::<Line>().unwrap(),
+        Line {
+            start: Point { x: 2, y: 0 },
+            end: Point { x: 6, y: 4 },
+        }
+    );
 }
 
 #[test]
 fn test_parse_point() {
-  assert_eq!(parse_line("2,1 -> 2,1"), Ok(Line {
-    x1: 2,
-    y1: 1,
-    x2: 2,
-    y2: 1,
-  }));
+    assert_eq!(
+        "2,1 -> 2,1".parse::<Line>().unwrap(),
+        Line {
+            start: Point { x: 2, y: 1 },
+            end: Point { x: 2, y: 1 },
+        }
+    );
 }
 
 #[test]
 fn test_line_to_points() {
-  let line = Line {x1: 1, y1: 1, x2: 3, y2: 3};
-  let expected = vec![
-    Point {x: 1, y: 1},
-    Point {x: 2, y: 2},
-    Point {x: 3, y: 3},
-  ];
-  assert_eq!(line.points(), expected)
+    let line = Line {
+        start: Point { x: 1, y: 1 },
+        end: Point { x: 1, y: 3 },
+    };
+    let expected = vec![
+        Point { x: 1, y: 1 },
+        Point { x: 1, y: 2 },
+        Point { x: 1, y: 3 },
+    ];
+    assert_eq!(line.points().unwrap().collect::<Vec<_>>(), expected)
 }
 
 #[test]
 fn test_point_to_points() {
-  let line = Line {x1: 2, y1: 1, x2: 2, y2: 1};
-  let expected = vec![
-    Point {x: 2, y: 1},
-  ];
-  assert_eq!(line.points(), expected)
+    let line = Line {
+        start: Point { x: 2, y: 1 },
+        end: Point { x: 2, y: 1 },
+    };
+    let expected = vec![Point { x: 2, y: 1 }];
+    assert_eq!(line.points().unwrap().collect::<Vec<_>>(), expected)
 }
